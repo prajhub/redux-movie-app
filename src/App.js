69edc8ca923404c10ca4e35827e0faf60e94d5fb
Prajwal Magar <pzlmagar@gmail.com>
2023-01-11 18:01:52 +0545

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.scss';
+import  Home  from "./component/Home/Home";
+import  Header  from "./component/Header/Header";
+import  Footer  from "./component/Footer/Footer";
+import  PageNotFound  from "./component/PageNotFound/PageNotFound"
+import  MovieDetail  from "./component/MovieDetail/MovieDetail"
 
 function App() {
   return (
-    <div className="App">
-        App
+    <div className='app'>
+      <Router>
+        <Header></Header> 
+        
+          <Route path="/" component={Home} />
+          <Route path='/movie/:imdbID' component={MovieDetail} />
+          <Route component={PageNotFound} />
+        
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/common/apis/movieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "488fb919";
